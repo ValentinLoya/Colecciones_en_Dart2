85ce98914bbe856ec8836a514a9f798084e73541
Valentin Loya <a.22308051281244@cbtis128.edu.mx>
2025-02-12 06:49:49 +0000

    Solucionado

---
+++ lista1.dart
@@ -0,0 +1,8 @@
+void main() {  
+   // lista tipo entero de 3 elementos
+   print(", Matricula: , gpo 6 J");
+   List<int> numeros = [10, 20, 30];
+   print("Lista de numeros enteros: ");
+   print(numeros);
+   print("Primer elemento: ${numeros[0]}");
+}
